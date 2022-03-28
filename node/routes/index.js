611f21f55bbd712028b845b07d52ce432b52c0b4
krejzic <tkrejza@gmail.com>
2022-03-28 13:02:18 +0200

    Update index.js

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Tohle je novinka' });
+  res.render('index', { title: 'Ryba je ryba' });
 });
 
 module.exports = router;
